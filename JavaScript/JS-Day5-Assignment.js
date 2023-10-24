9447a6fa952ac3b05ddc58cfa73d61ad508c2dbf
ANKU <ankitadixit1122@gmail.com>
2023-10-23 19:11:26 -0500

    JS-Day5-Assignment

---
+++ JavaScript/JS-Day5-Assignment.js
@@ -0,0 +1,9 @@
+function empDetails(){
+    let details ={
+        fname:document.getElementById("firstname").value,
+        lname:document.getElementById("lastname").value,
+        email:document.getElementById("email").value
+
+    }
+    console.log(details);
+}
