1f9f6f8e61dbe0a666a80bf6d5bf6e855d030caf
ANKU <ankitadixit1122@gmail.com>
2023-11-03 15:03:08 -0500

    adding ajax script

--- JavaScript/JavaScriptPromise/data.js
+++ JavaScript/JavaScriptPromise/data.js
@@ -20,3 +20,41 @@ fetch("https://gorest.co.in/public/v2/users").then((data)=>{
     console.log(err)
 })  
 }
+
+
+
+/*
+
+function getData(){
+    $.ajax({
+        url:'https://gorest.co.in/public/v2/users',
+        dataType:'json',
+        type:'GET',
+        data:{
+            key:'value'
+        },
+    success:data=>{
+        console.log(data)
+        if (data.length > 0) {
+        var temp = "";
+        data.forEach((itemData) => {
+        temp += "<tr>";
+        temp += "<td>" + itemData.id + "</td>";//getting from json (id,name...)
+        temp += "<td>" + itemData.name + "</td>";
+        temp += "<td>" + itemData.gender + "</td>";
+        temp += "<td>" + itemData.email + "</td>";
+        temp += "<td>" + itemData.status + "</td>";
+    });
+        //document.getElementById('data').innerHTML = temp;
+        $(document).ready(function(){
+        $("#data").html(temp)
+        })
+        }   
+    },
+    error:err=>{
+        console.log(err)
+        }
+    })
+}
+
+*/
